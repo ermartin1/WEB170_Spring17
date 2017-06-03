2e24988a8c497b62ba1137c7756976b258d1516f
Erin R Martin <erinrmartin@yahoo.com>
2017-06-02 23:48:52 -0700

    SEO and Meta Data

--- template/functions.php
+++ template/functions.php
@@ -15,13 +15,16 @@ register_nav_menus(array('main-menu' => 'Main Menu', 'footer-menu' => 'Footer Me
 
 
 //register my sidebar
-register_sidebars(2, array(
+register_sidebars(2, array('name' => 'Sidebar', 
+/*    'name'          => 'About right sidebar',
+    'id'            => 'about_right_1',
+    'id'            => 'about_right_2',*/
     'before_widget' => '<div id="%1$s" class="widget %2$s">',
     'after_widget' => '</div>',
     'before_title' => '<h2>',
     'after_title' => '</h2>',
 ));
-//to give a sidbar a name, place after after_widget register_sidebar(array('name =>'type in name'));
+//to give a sidebar a name, place after after_widget register_sidebar(array('name =>'type in name'));
 
 //create post thumbnails
 add_theme_support('post-thumbnails');
@@ -30,5 +33,42 @@ add_theme_support('post-thumbnails');
 //create page excerpts
 add_post_type_support('page', 'excerpt');
 
+
+//title tag function
+function get_my_title_tag() {
+    
+    global $post;
+    
+    if (is_front_page() || is_home()) { //if on the front page
+        
+        bloginfo('description'); //get the site tag line
+        
+    }
+      // || means or
+    elseif (is_page() || is_single() ) { //for site pages or postings 
+        
+        echo get_the_title($post->ID); //get the title for the pages
+       
+    }
+    
+    else { //for everything else (404 or search)
+        
+        bloginfo('description'); //get the site tagline
+        
+    }
+    
+    if ($post->post_parent) { //for site parent page
+        
+        echo ' | '; //seperator for spacing
+        echo get_the_title($post->post_parent); //get parent pg title
+    }
+    
+    
+    echo ' | '; //seperator for spacing
+    bloginfo('name'); //get the site name
+    echo ' | '; //seperator for spacing
+    echo 'Seattle, WA.'; //location goes here
+    
+} //end of seo/ meta data function
 
 ?>
--- template/header.php
+++ template/header.php
@@ -2,8 +2,15 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<title><?php bloginfo('description'); ?> | <?php bloginfo('name'); ?></title>
+<title><?php get_my_title_tag(); ?></title>
 
+<!--meta data starts-->
+
+<meta name="description" content="<?php echo strip_tags(get_the_excerpt()); ?>" />
+<meta name="description" content="De` Cafe` is a Seattle based decaffeinated coffee shop and roastery. We carry many of the local coffee shops decaf coffees as well as our own branded coffee."/>
+    
+ <!--meta data ends-->   
+    
 <!--Remy Sharp Shim --> 
 <!--[if lte IE 9]> 
 <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js" >
